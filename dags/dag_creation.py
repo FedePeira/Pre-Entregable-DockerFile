f4a7a7965f6c74a67f5ee28a4ff25d1dba86761c
FedePeira <fedepr2345@gmail.com>
2024-09-02 12:38:54 -0400

    Version 1.2

--- dags/dag_creation.py
+++ dags/dag_creation.py
@@ -1,58 +1,41 @@
-from datetime import datetime, timedelta
-from email.policy import default
-
+from datetime import timedelta
 from airflow import DAG
-from airflow.providers.common.sql.operators.sql import SQLExecuteQueryOperator
+from airflow.operators.python_operator import PythonOperator
+from airflow.utils.dates import days_ago
+from modules.anonymize import DataRetriever
+from modules.data_con import load_data
 
-default_args={
-    'owner': 'anderson_oca',
-    'retries':3,
-    'retry_delay': timedelta(minutes=5)
+default_args = {
+    'owner': 'federico_peirano',
+    'retries': 3,
+    'retry_delay': timedelta(minutes=5),
+    'catchup': False,
+    'start_date': days_ago(1),
 }
 
-DAG_ID = "postgres_operator_dag"
+dag_id = "postgres_operator_dag"
+description = "Dag conexion a postgres"
+schedule_interval = "@once"
 
-with DAG(
+dag = DAG(
+    dag_id=dag_id,
+    description=description,
+    schedule_interval=schedule_interval,
     default_args=default_args,
-    dag_id=DAG_ID,
-    description= 'Dag conexion a postgres',
-    start_date=datetime(2024,6,26),
-    schedule_interval='@once',
-    catchup=False
-    ) as dag:
+)
 
+# Task: Retrieve data from the API
+retrieve_data = DataRetriever()
+data = retrieve_data.retrieve_data()
 
+# Task: Anonymize the retrieved data
+anonymized_data = retrieve_data.anonymize_data()
 
-    create_table = SQLExecuteQueryOperator(
-        task_id="create_table",
-        sql="""
-            CREATE TABLE IF NOT EXISTS fin_mundo (
-            dt DATE,
-            pais VARCHAR(30)
-            );
-          """,
-           conn_id="postgres_operator_dag"
-        )
+load_data_task = PythonOperator(
+    task_id="load_data_to_redshift",
+    python_callable=load_data,
+    op_kwargs={"df": anonymized_data},
+    dag=dag,
+)
 
-
-    populate_data = SQLExecuteQueryOperator(
-        task_id='insertar_en_tabla',
-        sql="""
-            INSERT INTO fin_mundo 
-            (dt,pais)
-            values 
-                ('2025-12-12', 'Colombia'),
-                ('2035-08-15', 'Brasil'),
-                ('2030-09-21', 'Argentina'),
-                ('2045-07-13', 'Chile'),
-                ('2028-11-17', 'Ecuador'),
-                ('2032-03-19', 'Peru'),
-                ('2026-08-18', 'Uruguay'),
-                ('2037-05-22', 'Paraguay'),
-                ('2080-12-12', 'Venezuela'),
-                ('2071-12-12', 'Mexico');
-        """,
-        conn_id="postgres_operator_dag"
-    )
-    
-    create_table >> populate_data
+retrieve_data >> load_data_task
